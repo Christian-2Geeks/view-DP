515639bcd7a6c0f1873374e747704a16c96c8dc1
Christian Fernandez <christian.alex.94.94@gmail.com>
2021-09-06 11:48:32 -0400

    V 1.0

---
+++ src/utils/index.js
@@ -0,0 +1,66 @@
+(async function load() {
+  const URL = '../initialState.json';
+
+  const getData = async url => {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (Object.keys(data).length > 0) {
+      return data;
+    }
+    throw new Error('No se encontro ningun resultado');
+  };
+
+  const select = document.querySelector('#select');
+
+  const template = (departamento, users) => {
+    return `
+      <div class="area__table">
+        <h3>${departamento}</h3>
+        <div>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Coorporativo</th>
+                <th>Local</th>
+                <th>Extensión</th>
+              </tr>
+            </thead>
+            <tbody>
+            ${users
+              .map(user => {
+                return `
+                <tr>
+                  <td>${user.nombre}</td>
+                  <td>${user.coorporativo}</td>
+                  <td>${user.local}</td>
+                  <td>${user.extension}</td>
+                </tr>
+              `;
+              })
+              .join('')}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    `;
+  };
+
+  select.addEventListener('change', async event => {
+    const $directorioArea = document.querySelector('.directorio__area');
+    const $tables = $directorioArea.querySelector('#tables');
+    const data = await getData(URL);
+    let html = '';
+
+    Object.values(data).forEach((item, i) => {
+      if (item[i]) {
+        const HTMLString = template(Object.keys(data)[i], item);
+        html += HTMLString;
+      }
+    });
+    $tables.innerHTML = html;
+    if (!$directorioArea.classList.contains('active')) {
+      $directorioArea.classList.add('active');
+    }
+  });
+})();
